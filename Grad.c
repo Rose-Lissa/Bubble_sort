e0199b1f239d0b756a6b85f0aa754bf13bd27b5c
Rose-Lissa <v.romanenko@g.nsu.ru>
2021-09-29 01:28:17 +0700

    first commit

---
+++ Grad.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+int main() {
+	int N;
+	scanf("%d", &N);
+	int* array = (int*)malloc(sizeof(int) * N);
+	for (int i = 0; i < N; i++) {
+		array[i] = rand() % 10000;
+	}
+	int tmp;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < (N - 1); j++) {
+			if (array[j] > array[j + 1]) {
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		printf("%d ", array[i]);
+	}
+	return 0;
+}
